7ade8abdbc58e83e76aa9e365b0eed6b2fd1bda3
wesley.moody <wgm3190@gmail.com>
2019-11-15 10:18:24 -0600

    Tito the tyrannosaurus

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -7,7 +7,13 @@
 */
 
 // tyrannosaurus, carnivorous, 7000kg, 12m, Late Cretaceous
-
+const: tyrannosaurus = {
+  name: "Tito",
+  diet: "carnivorous",
+  weight: "7000kg",
+  length: "12m",
+  period: "Late Cretaceous"
+}
 // stegosaurus, herbivorous, 2000kg, 9m, Late Jurassic
 
 // velociraptor, carnivorous, 15kg, 1.8m, Late Cretaceous
