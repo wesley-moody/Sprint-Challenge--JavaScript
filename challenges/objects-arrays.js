bc219351de3b61edbaf3a833d352aab3a98746ea
wesley.moody <wgm3190@gmail.com>
2019-11-15 11:34:15 -0600

    array method request 2

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -91,7 +91,11 @@ The resulting contact information strings should have a space between the first 
 
 Log the result of your new array. */
 const contactInfo = [];
-function studentEmail()
+function gradInfo(graduates, index) {
+    const contacts = graduates(index);
+    contactInfo.push(gradInfo);
+    return `${contacts.first_name} ${contacts.email}`;
+}
 console.log(contactInfo);
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called unisWithUni that contains them all. This will be an array of objects. Log the result. */
